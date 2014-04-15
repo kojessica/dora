017583e77259d6eb23aab638f4344996aa84fbce
kojessica <ko.jessica@gmail.com>
2014-04-14 23:32:18 -0700

    create new group from search results page

--- dora/SearchResultsViewController.m
+++ dora/SearchResultsViewController.m
@@ -9,12 +9,17 @@
 #import "SearchResultsViewController.h"
 #import <Parse/Parse.h>
 #import "CustomSearchCell.h"
+#import "GroupDetailViewController.h"
+#import "Group.h"
 
 @interface SearchResultsViewController ()
 
 @property (strong,nonatomic) NSArray *groupsNames;
 @property (strong,nonatomic) NSArray *suggestedGroups;
+@property (nonatomic,assign) BOOL hasResults;
+@property (nonatomic,strong) NSString *needToCreate;
 - (void)reloadResults;
+- (void)createNewGroup;
 - (void)fetchAutoComplete:(NSString *)searchString;
 
 @end
@@ -37,16 +42,20 @@
     self.resultsTable.delegate = self;
     self.resultsTable.dataSource = self;
     self.searchInputBox.delegate = self;
-    
+    self.hasResults = YES;
     
     UINib* myCellNib = [UINib nibWithNibName:@"CustomSearchCell" bundle:nil];
     [self.resultsTable registerNib:myCellNib forCellReuseIdentifier:@"CustomSearchCell"];
     
-    [self allGroups];
     [self reloadResults];
+    [self allGroups];
+    
     self.resultsTable.separatorStyle = UITableViewCellSeparatorStyleNone;
     
     [self.searchInputBox becomeFirstResponder];
+    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+    self.searchInputBox.leftView = paddingView;
+    self.searchInputBox.leftViewMode = UITextFieldViewModeAlways;
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
@@ -60,6 +69,7 @@
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
+    NSLog(@"ending");
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
@@ -74,6 +84,17 @@
 {
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF BEGINSWITH[cd] %@", searchString];
     self.suggestedGroups = [self.groupsNames filteredArrayUsingPredicate:predicate];
+    
+    if ([self.suggestedGroups count] == 0) {
+        if ([searchString length] > 0) {
+            self.hasResults = NO;
+            self.needToCreate = searchString;
+        } else {
+            self.hasResults = YES;
+        }
+    } else {
+        self.hasResults = YES;
+    }
     NSLog(@"%@", self.suggestedGroups);
     [self reloadResults];
 }
@@ -123,12 +144,21 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.suggestedGroups count];
+    if (self.hasResults) {
+        return [self.suggestedGroups count];
+    } else {
+        return 1;
+    }
+    
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 60;
+    if (self.hasResults) {
+        return 60;
+    } else {
+        return 200;
+    }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
@@ -137,12 +167,45 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    CustomSearchCell *cell = (CustomSearchCell *)[tableView dequeueReusableCellWithIdentifier:@"CustomSearchCell"];
-    cell.name.text = [self.suggestedGroups objectAtIndex:indexPath.row];
-    cell.backgroundColor = [UIColor clearColor];
-    cell.name.textColor = [UIColor whiteColor];
-    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-    return cell;
+    if (self.hasResults) {
+        CustomSearchCell *cell = (CustomSearchCell *)[tableView dequeueReusableCellWithIdentifier:@"CustomSearchCell"];
+        cell.name.text = [self.suggestedGroups objectAtIndex:indexPath.row];
+        cell.backgroundColor = [UIColor clearColor];
+        cell.name.textColor = [UIColor whiteColor];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        return cell;
+    } else {
+        UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"TableCell"];
+        
+        UILabel *noResultsText = [[UILabel alloc] initWithFrame:(CGRectMake(0, 0, self.view.frame.size.width, 50.f))];
+        [noResultsText setText:[NSString stringWithFormat:@"We can't find %@", self.needToCreate]];
+        noResultsText.textColor = [UIColor colorWithRed:146/255.f green:146/255.f blue:146/255.f alpha:1];
+        noResultsText.textAlignment = NSTextAlignmentCenter;
+        
+        UIButton *createButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        createButton.frame = CGRectMake(70.f, 50.f, 180.f, 40.f);
+        [createButton setTitle:[NSString stringWithFormat:@"Create @%@", self.needToCreate] forState:UIControlStateNormal];
+        createButton.backgroundColor = [UIColor colorWithRed:40/255.f green:169/255.f blue:188/255.f alpha:1];
+        [createButton addTarget:self action:@selector(createNewGroup) forControlEvents:UIControlEventTouchUpInside];
+        createButton.titleLabel.font = [UIFont boldSystemFontOfSize:17];
+        
+        [cell addSubview:noResultsText];
+        [cell addSubview:createButton];
+        cell.backgroundColor = [UIColor clearColor];
+        return cell;
+    }
+}
+
+- (void)createNewGroup
+{
+    //TODO(jessica): create a new group in Parse
+    Group *groupToCreate = [[Group alloc] init];
+    groupToCreate.name = self.needToCreate;
+    GroupDetailViewController *groupDetailView = [[GroupDetailViewController alloc] init];
+    groupDetailView.group = groupToCreate;
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"dismissKeyboard" object:nil];
+    [self presentViewController:groupDetailView animated:YES completion:nil];
 }
 
 
