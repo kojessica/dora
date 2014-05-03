139bd7aeb8185b3eda4bdcfaf2742c0d64b24a04
Ben Chang <mrclam@gmail.com>
2014-05-03 16:13:35 -0700

    Introduce group detail collection view layout

---
+++ dora/GroupDetailCollectionViewLayout.h
@@ -0,0 +1,17 @@
+//
+//  GroupDetailCollectionViewLayout.h
+//  dora
+//
+//  Created by Benjamin Chang on 5/3/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupDetailCollectionViewLayout : UICollectionViewFlowLayout
+@property (nonatomic, assign) CGPoint center;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) NSInteger cellCount;
+
+
+@end
---
+++ dora/GroupDetailCollectionViewLayout.m
@@ -0,0 +1,59 @@
+//
+//  GroupDetailCollectionViewLayout.m
+//  dora
+//
+//  Created by Benjamin Chang on 5/3/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import "GroupDetailCollectionViewLayout.h"
+@interface GroupDetailCollectionViewLayout()
+
+// arrays to keep track of insert, delete index paths
+@property (nonatomic, strong) NSMutableArray *deleteIndexPaths;
+@property (nonatomic, strong) NSMutableArray *insertIndexPaths;
+
+@end
+
+
+@implementation GroupDetailCollectionViewLayout
+- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    // Must call super
+    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.insertIndexPaths containsObject:itemIndexPath])
+    {
+        // only change attributes on inserted cells
+       // if (!attributes)
+            attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        
+        // Configure attributes ...
+        attributes.alpha = 0.0;
+        attributes.center = CGPointMake(_center.x, _center.y);
+    }
+    
+    return attributes;
+}
+- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    // So far, calling super hasn't been strictly necessary here, but leaving it in
+    // for good measure
+    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.deleteIndexPaths containsObject:itemIndexPath])
+    {
+        // only change attributes on deleted cells
+        if (!attributes)
+            attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        
+        // Configure attributes ...
+        attributes.alpha = 0.0;
+        attributes.center = CGPointMake(_center.x, _center.y);
+        attributes.transform3D = CATransform3DMakeScale(0.1, 0.1, 1.0);
+    }
+    
+    return attributes;
+}
+
+@end
